695c0aacf9ffb6a34840abb484c9e7207bc4e67d
djaco <jdavis88@arizona.edu>
2022-09-23 13:50:23 -0700

    got it roughly working

--- pa4/sgrep/sgrep.c
+++ pa4/sgrep/sgrep.c
@@ -12,7 +12,7 @@ Purpose:
 
 int checkFlags(char flagStr, int * e, int * o, int * i){
   if(flagStr == 'e'){
-    *c = 1;
+    *e = 1;
     return 0;
   } else if(flagStr == 'o'){
     *o = 1;
@@ -56,20 +56,30 @@ int argOps(int argc, char * argv[], int * searchWordIndex, int * eFlag,
 
 
 
-int match(int lineIndex, char line[], char phrase, int e, int i){
+int match(int lineIndex, char line[], char phrase[], int e, int i){
   int phraseIndex = 1;
-  while(line[lineIndex] != '\n'){
-    if (phrase[phraseIndex] == '\n'){
+  while(line[lineIndex] != '\0'){
+    if (phrase[phraseIndex] == '\0'){
       return lineIndex;
     }
     else if (e == 1 && phrase[phraseIndex] == '#' && 
-             phrase[phraseIndex+1] != '\n'){
+             phrase[phraseIndex+1] != '\0'){
       phraseIndex++;
-      while(line[lineIndex] != '\n' && line[lineIndex] != phrase[phraseIndex]){
-    //UNDER CONSTRUCTION!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!    
+      while(line[lineIndex] != '\0'){
+        if (line[lineIndex] != phrase[phraseIndex]){
+          phraseIndex++;
+          break;
+        }
+        lineIndex++;
       }
+    } else if(line[lineIndex] == '.' && e == 1){
+      phraseIndex++;
+    } else if(line[lineIndex] == phrase[phraseIndex]){
+      phraseIndex++;
     }
+    lineIndex++;
   }
+  return -1;
 } 
 
 
@@ -81,36 +91,55 @@ int findOcc(char phrase[], int e, int o, int i, char line[],
   int index = 0;
   
   while(index<strlen(line)){
-    if (line[index] == '\n'){
+    if (line[index] == '\0'){
       *stop = index;
       break;
+    } 
+    int result = -1;
+    if (line[index] == phrase[0]){
+      result = match(index, line, phrase, e, i);
+      if(result != -1){
+        if (o == 1){
+          for (int j = index; j <= result; j++){
+            printf("%c", line[j]);
+          }
+        } else {
+          printf("%s", line);
+        }
+        printf("\n");
+        return 0;
+      }
     }
-    //test999999999999999999999999999999999999999999999999999999999
-    printf("%c",line[index]);
     index++;
   }
-  printf("\n");
-  return 0;
+  return 1;
 }
 
 
 
 
-int checkPhrase(char phrase[]){
+int checkPhrase(char phrase[], int e){
   int index = 0;
   int flag = 0; //flag to check if there are two # in a row
-  while(phrase[index] !='\n'){
+  while(phrase[index] !='\0'){
+    printf("%c", phrase[index]);
     // makes sure the input is '.','#', char, or int
     if (phrase[index] != '.' && phrase[index] != '#'){
       if(phrase[index] < 'a' || phrase[index] > 'z'){
         if(phrase[index] > 'Z' || phrase[index] < 'A'){
           if (phrase[index] < '0' || phrase[index] > '9'){
+            printf("%c", phrase[index]);
+            printf("HERE");
             return 1;
           }
+          // if we dont want a number after/before a #:...............................................
+          // flag = 1
         }
       }
-    }else if (phrase[index] == '#'){
-      if (index == 0 || phrase[index + 1] == '\n')
+    // if we accept '#' and '.' without the -e flag ..................................................
+    // the next two if statements need to include '&& e == 1'.........................................
+    }else if(phrase[index] == '#'){
+      if (index == 0 || e == 0 || phrase[index + 1] == '\0'){
         return 1;
       } else if (flag == 1) {
         return 1;
@@ -118,7 +147,7 @@ int checkPhrase(char phrase[]){
         flag = 1;
       }
     }else if (phrase[index] == '.'){
-      if (flag == 1){
+      if (flag == 1 || e == 0){
         return 1;
       } else {
         flag = 1;
@@ -149,7 +178,7 @@ int main(int argc, char * argv[]){
   // also identifies any input errors
   if (argOps(argc, argv, &searchWordIndex, &eFlag, &oFlag, &iFlag) == 1){
     return 1;
-  } else if (checkPhrase(argv[searchWordIndex]) == 1){
+  } else if (checkPhrase(argv[searchWordIndex], eFlag) == 1){
     printf("Invalid search term.\n");
     return 2;
   }
