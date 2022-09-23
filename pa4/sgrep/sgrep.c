0a525dd9590915714b496a68d9671804b52db3b3
djaco <jdavis88@arizona.edu>
2022-09-22 17:03:53 -0700

    added fgets to read, and started geting info from argv

--- pa4/sgrep/sgrep.c
+++ pa4/sgrep/sgrep.c
@@ -5,7 +5,31 @@ Purpose:
 
 #include <stdio.h>;
 
-int main(int argc, char * argv[]){                                                                                                                                                                                                            
+int getArgs(int argc, char * argv[], char funcs[], int * searchWordIndex){
+  for (int i = 1; i < argc; i++){
+    if (argv[i][0] == '-'){
+     
+
+
+
+
+
+int main(int argc, char * argv[])
+  char funcs[4];
+  funcs[0]='\n'; // flag to mark the end of the funtions
+  int searchWordIndex = -1;
+  
+  // decyphers argv[]
+  getArgs(argc, argv, funcs, &searchWordIndex) 
+  //get input and send to appropriate function
+  char buffer[129];
+  while (fgets(buffer, 128, stdin) != NULL){
+    if (delim == 'l'){
+      l(buffer, cols);
+    } else {
+      wAndC(buffer, cols, delim);
+    }
+  } 
   return 0;a
 }
 
