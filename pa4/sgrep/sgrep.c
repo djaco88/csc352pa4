bfc0cccc23c429ef45e3b8204dbabda1991b2f9b
djaco <jdavis88@arizona.edu>
2022-09-23 17:27:22 -0700

    commented the program

--- pa4/sgrep/sgrep.c
+++ pa4/sgrep/sgrep.c
@@ -1,6 +1,7 @@
 /*
 Author: Jacob Davis
-Purpose:
+Purpose: Simulate the grep bash command, supporting
+flags: -o, -e, -i
 */
 
 #include <stdio.h>
@@ -9,7 +10,9 @@ Purpose:
 
 
 
-
+// Checks to make sure only valid (e, o, i)
+// flags are given. Also stores 1 occurrence 
+// of the flag
 int checkFlags(char flagStr, int * e, int * o, int * i){
   if(flagStr == 'e'){
     *e = 1;
@@ -29,22 +32,29 @@ int checkFlags(char flagStr, int * e, int * o, int * i){
 
 
 
+// Goes through the given args and checks for correctness
+// Finds the index of the search phrase
 int argOps(int argc, char * argv[], int * searchWordIndex, int * eFlag,
            int * oFlag, int * iFlag){
   int searchWordFound = 0; // flag to make sure there is only one search phrase
 
   for (int i = 1; i < argc; i++){
+    // check flags
     if (argv[i][0] == '-'){
       if (strlen(argv[i]) != 2){
 	printf("ERROR: flags must be one character long\n");
         return 1; // true, there is an error
       } else if (checkFlags(argv[i][1], eFlag, oFlag, iFlag) == 1){
-        return 1; // 1 is ture, there is an error
+        return 1; // 1 is true, there is an error
       }
-    } else if (searchWordFound == 1){
+    } 
+    // checks that there is only one search phrase
+    else if (searchWordFound == 1){
       printf("ERROR: there can only be flags, one search phrase, and one file\n");
       return 1; // true, there was an error
-    } else {
+    } 
+    // find the searchword
+    else {
       searchWordFound = 1;
       *searchWordIndex = i;
     } 
@@ -54,6 +64,9 @@ int argOps(int argc, char * argv[], int * searchWordIndex, int * eFlag,
 
 
 
+
+
+// Turns any lowercase letter into a capital letter
 char allCaps(char x){
   if (x >= 97 && 122 >= x){
       x = x - 32;
@@ -64,9 +77,16 @@ char allCaps(char x){
 
 
 
+
+// if the first letter of the search phrase is found
+// this function will loop through the current line
+// and search phrase for a match. All -e special
+// characters are handled here
 int match(int lineIndex, char line[], char phrase[], int e, int i){
   int phraseIndex = 1;
+
   while(line[lineIndex] != '\0'){
+    // handle the -i case
     char lineChar = line[lineIndex];
     char phraseChar = phrase[phraseIndex];
     if (i == 1){
@@ -74,17 +94,22 @@ int match(int lineIndex, char line[], char phrase[], int e, int i){
       phraseChar = allCaps(phraseChar);
     }
 
+    // matched word found, returns the 
+    // current index of the line
     if (phrase[phraseIndex] == '\0'){
       return lineIndex;
     }
 
+    // handles -e '#' case
     else if (e == 1 && phraseChar == '#' && 
              phrase[phraseIndex+1] != '\0'){
+      // update -i case
       phraseIndex++;
       phraseChar = phrase[phraseIndex];
       if (i == 1){
 	phraseChar = allCaps(phraseChar);
       }
+      // loop until mismatch or match
       while(lineChar != '\0'){
         if (lineChar == phraseChar){
           phraseIndex++;
@@ -92,47 +117,59 @@ int match(int lineIndex, char line[], char phrase[], int e, int i){
           break;
         }
         lineIndex++;
+        // -i case
         lineChar = line[lineIndex];
         if (i == 1){
           lineChar = allCaps(lineChar);
         }
+        // returns special value if the end of the line
+        // is hit with no matches.
+        // ends all searches on the current line
         if(lineChar == '\0'){
           return -2;
         } 
       }
     } 
-
+ 
+    // -e '.' case
     else if(phraseChar == '.' && e == 1){
       phraseIndex++;
       lineIndex++;
     } 
 
+
+    // normal match
     else if(lineChar == phraseChar){
       phraseIndex++;
       lineIndex++;
     } 
-
+  
+    // no match
     else {
       return -1;
     }
   }
+  // end of file hit, no match
   return -1;
 } 
 
 
 
 
-
-int findOcc(char phrase[], int e, int o, int i, char line[],
-            int * start, int * stop){
+// main searching function
+// loops through the entire line
+// to find potential matches
+// prints results as they appear
+int findOcc(char phrase[], int e, int o, int i, char line[]){
   int index = 0;
   
   while(index<strlen(line)){
-    if (line[index] == '\0'){
-      *stop = index;
+    // end of line
+    if (line[index] == '\n'){
       break;
     } 
-    int result = -1;
+    
+    // -i case
     char lineChar = line[index];
     char phraseChar = phrase[0];
     if (i == 1){
@@ -140,19 +177,27 @@ int findOcc(char phrase[], int e, int o, int i, char line[],
       phraseChar = allCaps(phraseChar);
     }
 
-
+    // check for first letter match
+    int result = -1;
     if (lineChar == phraseChar){
+      // send to match()
       result = match(index+1, line, phrase, e, i);
+      // special case: # hit the end of the line
+      // no possible matches 
       if (result == -2){
         return 0;
       }
+      // match
       if(result != -1){
+        // -o case, print and continue
         if (o == 1){
           for (int j = index; j < result; j++){
             printf("%c", line[j]);
           }
           printf("\n");
           index = result;
+        // normal case, print whole line and
+        // move on to the next line
         } else {
           printf("%s", line);
           return 0;
@@ -170,6 +215,7 @@ int findOcc(char phrase[], int e, int o, int i, char line[],
 int checkPhrase(char phrase[], int e){
   int index = 0;
   int flag = 0; //flag to check if there are two # in a row
+
   while(phrase[index] !='\0'){
     // makes sure the input is '.','#', char, or int
     if (phrase[index] != '.' && phrase[index] != '#'){
@@ -179,13 +225,12 @@ int checkPhrase(char phrase[], int e){
             printf("HERE");
             return 1;
           }
-          // if we dont want a number after/before a #:...............................................
+          // if we dont want a number after/before a #:
           // flag = 1
         }
       }
       flag = 0;
-    // if we accept '#' and '.' without the -e flag ..................................................
-    // the next two if statements need to include '&& e == 1'.........................................
+    // check special characters for -e restrictions
     }else if(phrase[index] == '#'){
       if (index == 0 || e == 0 || phrase[index + 1] == '\0'){
         return 1;
@@ -214,10 +259,7 @@ int checkPhrase(char phrase[], int e){
 
 
 
-
-
-
-
+// MAIN
 int main(int argc, char * argv[]){
   if (argc < 2){
     printf("Requires more command-line arguments.\n");
@@ -227,10 +269,8 @@ int main(int argc, char * argv[]){
   int eFlag = 0;
   int oFlag = 0;
   int iFlag = 0;
-   
-  // decyphers argv[]. put flags in an array
-  // and get the index of search phrase
-  // also identifies any input errors
+  
+  // error handling and arg ops 
   if (argOps(argc, argv, &searchWordIndex, &eFlag, &oFlag, &iFlag) == 1){
     return 1;
   } else if (checkPhrase(argv[searchWordIndex], eFlag) == 1){
@@ -238,15 +278,10 @@ int main(int argc, char * argv[]){
     return 2;
   }
   
+  // try to find matches for input
   char buffer[129];
   while (fgets(buffer, 128, stdin) != NULL){
-    // create indices to return a section of the
-    // current line
-    int start = 0;
-    int stop = 127;
-
-    findOcc(argv[searchWordIndex], eFlag, oFlag, iFlag, buffer,
-            &start, &stop);
+    findOcc(argv[searchWordIndex], eFlag, oFlag, iFlag, buffer);
   }
   return 0;
 }
